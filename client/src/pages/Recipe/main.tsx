938f8b749417d65b366e96cd76a558207f68e505
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2023-04-16 17:30:06 +0400

    c

--- client/src/pages/Recipe/main.tsx
+++ client/src/pages/Recipe/main.tsx
@@ -1,9 +1,16 @@
 import React, { FormEvent, useState } from 'react'
 import { fetchData } from '../../utils'
 
+
+
 const Recipe = () => {
+  const [name, setName] = useState<string>('')
+
+
   const [response, setResponse] = useState<any>(null)
+  const [generatedImg, setGeneratedImg] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [isImgLoading, setIsImgLoading] = useState(false)
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
@@ -16,14 +23,26 @@ const Recipe = () => {
   }
 
   return (
-    <main className='text-white'>
+    <main className='text-white section'>
       <h1>Make Recipe:</h1>
       <form onSubmit={handleSubmit}>
-        {response !== null && (
-          response.map(({ url }: { url: string }) => (
-            <img src={url} alt="" width={256} />
-          ))
-        )}
+        <label htmlFor='name'>
+          What do you want to cook:
+          <input
+            id='name'
+            type='text'
+            value={name}
+          />
+        </label>
+
+        <label htmlFor='ingredients'>
+          What do you want to cook:
+          <input
+            id='ingredients'
+            type='text'
+            value={name}
+          />
+        </label>
         <button>{isLoading ? 'loading' : 'submit'}</button>
       </form>
     </main>
