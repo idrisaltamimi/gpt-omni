48e43ea4e2bb5bb14bed4ce9e663d7c3dc4d2604
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2023-04-19 23:49:02 +0400

    c

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,10 +11,10 @@ const __dirname = path.dirname(__filename)
 dotenv.config()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, '../../client/build')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'))
 })
 
 const openAi = new OpenAIApi(
