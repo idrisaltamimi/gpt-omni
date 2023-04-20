631d07f7a6a8c934770b9b7f55ab57a01c7b91ff
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2023-04-20 10:36:31 +0400

    server host change + head title change

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -7,7 +7,7 @@ export function parseHtml(text: string) {
 }
 
 export function fetchData(method: string, input: string, endpoint?: string) {
-  return fetch(`gpt-omni-production.up.railway.app${endpoint || ''}`, {
+  return fetch(`https://pleasant-crow-garb.cyclic.app${endpoint || ''}`, {
     method: method,
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ content: input })
