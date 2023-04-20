cf3c8065a03add3c50c11fd664c4db28fb502b81
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2023-04-20 14:10:25 +0400

    c

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -7,7 +7,7 @@ export function parseHtml(text: string) {
 }
 
 export function fetchData(method: string, input: string, endpoint?: string) {
-  return fetch(`https://gpt-omni.onrender.com${endpoint || ''}`, {
+  return fetch(`https://gpt-omni.onrender.com${endpoint || '/'}`, {
     method: method,
     headers: { 'Content-Type': 'application/json' },
     mode: 'no-cors',
