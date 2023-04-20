193af02be4162800c34059ef2197bd4b3b1c884a
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2023-04-20 10:43:42 +0400

    add cors websites

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,8 +8,15 @@ const app: Application = express()
 const __filename = import.meta.url
 const __dirname = path.dirname(__filename)
 
+const originList = ['https://gpt-omni.netlify.app', 'http://localhost:5173']
+
+const corsOptions = {
+  origin: originList,
+  optionsSuccessStatus: 200
+}
+
 dotenv.config()
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../../client/build')))
 
