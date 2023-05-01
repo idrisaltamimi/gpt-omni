32e4c2166c84b02a7ba69bba692be59b45f426f1
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2023-05-01 09:37:32 +0400

    remove h1 tag

--- client/src/pages/Home/main.tsx
+++ client/src/pages/Home/main.tsx
@@ -15,7 +15,6 @@ const Home = () => {
 
   return (
     <main className='max-w-[50rem] w-full mx-auto px-4 flex flex-col justify-center items-center h-full'>
-      <h1 className='pt-10 pb-6 text-4xl font-bold text-white md:pt-0 md:text-6xl'>GPT-OMNI</h1>
       <div className='flex flex-col gap-4'>
         {links.map((link, index) => (
           <LinkCard
